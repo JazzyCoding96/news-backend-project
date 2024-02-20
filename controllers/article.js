8cd18f788cdae9404af7186b64ccd51ff0c68364
JazzyCoding96 <jazzysingh96@live.co.uk>
2024-02-20 16:52:28 +0000

    Added POST tests and endpoint

--- controllers/article.js
+++ controllers/article.js
@@ -1,11 +1,11 @@
-const { selectArticleById, fetchAllArticles, commentsByArticleId } = require("../models/article")
+const { selectArticleById, fetchAllArticles, commentsByArticleId, insertComment } = require("../models/article")
 
 
 exports.getArticleById = (req, res, next) => {
     
     const { article_id } = req.params;
     selectArticleById(article_id).then((article) => {
-        res.status(200).send(article)
+        res.status(200).send({ article })
     }).catch((err) => {
         next(err)
     })
@@ -13,7 +13,7 @@ exports.getArticleById = (req, res, next) => {
 
 exports.getAllArticles = (req, res, next) => {
   fetchAllArticles().then((articles) => {
-    res.status(200).send(articles);
+    res.status(200).send({ articles });
   });
 };
 
@@ -21,7 +21,22 @@ exports.getAllComments = (req, res, next) => {
   const { article_id } = req.params;
   commentsByArticleId(article_id)
     .then((comments) => {
-      res.status(200).send(comments);
+      res.status(200).send({ comments });
+    })
+    .catch((err) => next(err));
+};
+
+exports.addComment = (req, res, next) => {
+  const { article_id } = req.params;
+  const { body, username } = req.body;
+  if (!username.length || !body.length) {
+    return res
+      .status(400)
+      .send({ msg: "Username and body are required fields" });
+  }
+  insertComment(article_id, body, username)
+    .then((comment) => {
+      res.status(201).send({ comment });
     })
     .catch((err) => next(err));
 };
