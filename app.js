eec3e315a42abd88c0642845098c4507397aa399
JazzyCoding96 <jazzysingh96@live.co.uk>
2024-02-21 12:11:33 +0000

    Added Patch endpoint

--- app.js
+++ app.js
@@ -1,43 +1,49 @@
-const express = require('express');
-const { getAllTopics } = require('./controllers/topics');
-const { getEndpoints } = require('./controllers/endpoint');
-const { getArticleById, getAllArticles, getAllComments, addComment } = require('./controllers/article');
-const app = express()
+const express = require("express");
+const { getAllTopics } = require("./controllers/topics");
+const { getEndpoints } = require("./controllers/endpoint");
+const {
+  getArticleById,
+  getAllArticles,
+  getAllComments,
+  addComment,
+  patchArticle,
+} = require("./controllers/article");
+const app = express();
 
-
-app.use(express.json())
+app.use(express.json());
 
 app.get("/api/topics", getAllTopics);
 
-app.get("/api", getEndpoints)
+app.get("/api", getEndpoints);
 
-app.get("/api/articles/:article_id", getArticleById)
+app.get("/api/articles/:article_id", getArticleById);
 
-app.get("/api/articles/", getAllArticles)
+app.get("/api/articles/", getAllArticles);
 
-app.get("/api/articles/:article_id/comments", getAllComments)
+app.get("/api/articles/:article_id/comments", getAllComments);
 
-app.post("/api/articles/:article_id/comments", addComment)
+app.post("/api/articles/:article_id/comments", addComment);
 
+app.patch("/api/articles/:article_id", patchArticle);
 
 app.use((err, req, res, next) => {
   if (err.status && err.msg) {
     res.status(err.status).send({ msg: err.msg });
   }
-  
-  res.status(400).send();
+  next(err);
 });
-
-// Error handling middleware
+app.use((err, req, res, next) => {
+  if (err.code === "22P02") {
+    res.status(400).send({ msg: "Bad request" });
+  }
+  next(err);
+});
 app.use((err, req, res, next) => {
   res.status(404).send();
 });
 
 module.exports = app;
 
-
 /*
-
-Consider what errors could occur with this endpoint, and make sure to test for them.
 
 Remember to add a description of this endpoint to your /api endpoint.*/
